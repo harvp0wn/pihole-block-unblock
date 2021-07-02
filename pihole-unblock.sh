11cc5906f7a466d2f4c90301e3525f878353db0b
harvp0wn <32920063+harvp0wn@users.noreply.github.com>
2021-07-02 11:58:04 -0700

    Update pihole-unblock.sh

--- pihole-unblock.sh
+++ pihole-unblock.sh
@@ -2,7 +2,7 @@
 
 # Run as root (pihole requires it)
 
-# Default mode is to use whitelist. You can also switch to remove wildcard blacklist if you prefer (--wild -d)
+# Default mode to remove wildcard blacklist.
 mode="--wild -d"
 
 while read line; do /usr/local/bin/pihole $mode $line; done < /home/pi/pihole-block-unblock/list
