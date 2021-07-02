000e58719e0ea412693909304e67367793fc3ccf
harvp0wn <32920063+harvp0wn@users.noreply.github.com>
2021-07-02 11:59:12 -0700

    Create pihole-block.sh

--- pihole-block.sh
+++ pihole-block.sh
@@ -2,7 +2,7 @@
 
 # Run as root (pihole requires it)
 
-# Default is to use blacklist. You can also switch to wildcard blacklist if you prefer (--wild)
+# Default is to add to wildcard blacklist.
 mode="--wild"
 
 while read line; do /usr/local/bin/pihole $mode $line; done < /home/pi/pihole-block-unblock/list
